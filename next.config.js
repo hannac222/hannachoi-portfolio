30ce6d89eadfd6b236897b752e4ff6f513d17fb7
Hanna <hannac222@gmail.com>
2019-02-20 16:22:15 +0000

    Upgrade to Now 2.0

--- next.config.js
+++ next.config.js
@@ -1,2 +1,19 @@
-const withSass = require("@zeit/next-sass");
-module.exports = withSass();
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? {}
+    : require("next-server/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  const withSass = require("@zeit/next-sass");
+
+  return {
+    ...defaultConfig,
+    target: "serverless",
+    ...withSass()
+  };
+};
