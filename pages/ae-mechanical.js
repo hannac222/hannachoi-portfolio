7098741d5d14a068fbfca5c426a6e6d8ea795871
Hanna <hannac222@gmail.com>
2019-02-26 14:48:02 +0000

    Style AE Mechanical page

--- pages/ae-mechanical.js
+++ pages/ae-mechanical.js
@@ -2,64 +2,69 @@ import React from "react";
 import Layout from "../components/layout";
 import PagesHero from "../components/project-pages/pages-hero";
 import ProjectOverview from "../components/project-pages/project-overview";
-import ProjectDetail from "../components/project-pages/project-detail";
+import ProjectContent from "../components/project-pages/project-content";
+import ProjectContentDetail from "../components/project-pages/project-content__detail";
 import PageNav from "../components/page-nav";
 import SocialMedia from "../components/social-media";
 
 const AeMechanical = () => (
-  <Layout className="page-ae">
+  <Layout className="page-ae-mechanical">
     <PagesHero pageTitle="AE Mechanical" pageName="ae-mechanical" />
-    {/* <div className="page-top"> */}
-    {/* <span />
-        <div className="title">
-        <h1>AE Mechanical</h1>
-      </div> */}
-    {/* </div> */}
+
     <ProjectOverview
+      pageName="ae-mechanical"
       projectInfo="Plumbing company client"
       siteUrl="http://beta.aemechanical.ca"
-      technology="React.js | Next.js | Sass | Adobe XD | Responsive | SVG"
-      imgUrl="audra-wireframes.jpg"
-      alt="Audra Ricketts Wireframes"
+      technology="React.js | Next.js | Scss | Adobe XD | Responsive | SVG"
     />
-    <div className="wrapper-project">
-      <section className="project-container">
-        {/* <div className="project-bg" /> */}
-        <div className="project-content">
-          <div className="concept">
-            <h2>Concept</h2>
-            <p>
-              The purpose of this website is to showcase the client's past and
+    <div className="ae-video">
+      <img
+        src="/static/assets/ae-mechanical/imac.png"
+        alt="AE Mechanical demo video"
+      />
+      <span />
+      <video autoPlay muted loop>
+        <source
+          src="/static/assets/ae-mechanical/ae-mechanical-video.mp4"
+          type="video/mp4"
+        />
+      </video>
+    </div>
+
+    <ProjectContent
+      concept="The purpose of this website is to showcase the client's past and
               current artwork to galleries and interested buyers. It’s a place
               to learn about the client by reading her biography, artist
               statement and CV as well as browsing her process and contact her.
               To make her artworks stand out, it’s designed modern, clean,
-              minimalistic with a white background.
-            </p>
-          </div>
-          <div className="project-preview" />
-          <div className="development">
-            <h2>Development</h2>
-            <p>
-              For the content management system, we built it with Wordpress and
+              minimalistic with a white background."
+      pageName="ae-mechanical"
+      previewUrl="ae-preview.jpg"
+      previewAlt="AE Mechanical Preview"
+      development=" For the content management system, we built it with Wordpress and
               a customized theme and admin dashboard. With Lightbox for the
               gallery, users are able to look through images. The contact form
               is built with the Ninja form plug-in which automatically sends a
               confirmation email to users. To match the website design, we
-              customized Google Maps colors and style.
-            </p>
-          </div>
-        </div>
-        <div className="project-detail">
-          <ProjectDetail
-            imgUrl="audra-detail.jpg"
-            alt="Audra Ricketts Detail Pages"
-          />
-        </div>
-      </section>
-      <PageNav previousLink="/bcit-portfolio" nextLink="/lemongrass" />
-      <SocialMedia />
-    </div>
+              customized Google Maps colors and style."
+    />
+
+    <ProjectContentDetail
+      pageName="ae-mechanical"
+      detailUrl01="ae-mechanical-home.jpg"
+      detailAlt01="AE Mechanical website Home page"
+      detailUrl02="ae-mechanical-contact.jpg"
+      detailAlt02="AE Mechanical website Contact page"
+      detailUrl03="ae-mechanical-about.jpg"
+      detailAlt03="AE Mechanical website About page"
+      detailUrl04="ae-mechanical-services.jpg"
+      detailAlt04="AE Mechanical website Services page"
+      detailUrl05="ae-mechanical-faq.jpg"
+      detailAlt05="AE Mechanical website FAQ page"
+    />
+
+    <PageNav previousLink="/bcit-landing-page" nextLink="/audra-ricketts" />
+    <SocialMedia />
   </Layout>
 );
 
